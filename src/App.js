0afbdd336d99d77b065b11bebb1df22449a90b26
JuliaRa411 <777.07j@gmail..com>
2022-01-27 21:26:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import axios from "axios"
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  
+  state ={
+users:[]
+  }
+
+componentDidMount(){
+axios.get("https://jsonplaceholder.typicode.com/users")
+.then(res => {
+  console.log(res.data)
+  const users = res.data;
+  this.setState({users})
+})
+ }
+ render (){
+
+  return ( 
+    <div> 
+   {this.state.users.map(user => <p key={user.id}>  
+  <span className='green'> <span className='title'> Name :</span> {user.name} </span>
+  <span className='red'> <span className='title'> Email:</span>  {user.email} </span>
+<span className='blue'> <span className='title'>  Phone:</span> {user.phone} </span>
+<span className='orange'><span className='title'> Suite: </span>  {user.address.suite} </span>
+<span className='violet'> <span className='title'> City: </span>{user.address.city} </span>
+<span className='pink'> <span className='title'>  Street: </span>{user.address.street} </span>
+  
+  </p>
+ 
+     
+   )}
+
     </div>
-  );
+  )
+}
+
 }
 
 export default App;
